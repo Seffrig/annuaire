1f63b6d2b5270afd12c6bdb79c232ec4f9dddc5f
Seffrig <benoit.crivelli@gmail.com>
2015-01-16 16:01:38 +0100

    add a connexion line

--- commun/connexion_db.php
+++ commun/connexion_db.php
@@ -1,7 +1,8 @@
 <?php 
 	// connexion à la base
 //$dbconn = pg_connect("host=localhost dbname=annuaire user=postgres password=localhost"); 
-$dbconn = pg_connect("host=localhost dbname=annuaire user=elodie password=once-123"); 
+//$dbconn = pg_connect("host=localhost dbname=annuaire user=elodie password=once-123"); 
+$dbconn = pg_connect("host=localhost dbname=annuaire user=benoit password=postgresql"); 
 if (!$dbconn) 
 {  
 	echo "Une erreur s'est produite au chargement de la database.\n";  
